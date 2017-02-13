f10e0db7a149448ada6d9ba7bd255a53d4a4e424
sprinteiro <sprinteiro@gmail.com>
2017-02-13 22:55:17 +0000

    Add comment test2

--- src/main/java/org/openshift/InsultResource.java
+++ src/main/java/org/openshift/InsultResource.java
@@ -10,7 +10,7 @@ import javax.ws.rs.Produces;
 @RequestScoped
 @Path("/insult")
 public class InsultResource {
-// Comment test
+// Comment test 2
 	@GET()
 	@Produces("application/json")
 	public HashMap<String,String> getInsult() {
